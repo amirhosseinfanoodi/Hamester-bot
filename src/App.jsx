f2b5e3e3c776ff75d5b108f84b88297b4f7fa6a6
amirhosseinfanoodi <121581020+amirhosseinfanoodi@users.noreply.github.com>
2024-11-29 18:44:31 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+// import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+        <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-3xl font-bold">Hello Tailwind and React!</h1>
+       </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // مطمئن شوید که مسیر فایل‌های JSX یا TSX درست است
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
